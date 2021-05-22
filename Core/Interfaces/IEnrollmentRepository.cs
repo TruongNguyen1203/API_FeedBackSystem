7a82ab2fa64144e929e1b8efd869431be4978c0a
Thuyety <thuyety15@gmail.com>
2021-05-22 15:33:11 +0700

    edit code of enrollment

--- Core/Interfaces/IEnrollmentRepository.cs
+++ Core/Interfaces/IEnrollmentRepository.cs
@@ -7,9 +7,9 @@ namespace Core.Interfaces
     public interface IEnrollmentRepository
     {
         Task<IEnumerable<Enrollment>> GetEnrollments();
-        Task<Enrollment> GetEnrollment(int classId, int traineeId);
+        Task<Enrollment> GetEnrollment(int classId, string traineeId);
         Task<Enrollment> AddEnrollment(Enrollment enrollment);
         Task<Enrollment> UpdateEnrollment( Enrollment enrollment);
-        Task<Enrollment> DeleteEnrollment(int classId, int traineeId);
+        Task<Enrollment> DeleteEnrollment(int classId, string traineeId);
     }
 }
