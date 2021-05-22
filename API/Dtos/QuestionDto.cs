686aa76a006c3fcc59341a42199536a7f8c5c3f0
Thuyety <thuyety15@gmail.com>
2021-05-22 22:28:12 +0700

    Without delete method

---
+++ API/Dtos/QuestionDto.cs
@@ -0,0 +1,9 @@
+namespace API.Dtos
+{
+    public class QuestionDto
+    {
+        public int MyProperty { get; set; }
+        public int TopicID { get; set; }
+        public string QuestionContent { get; set; }
+    }
+}
