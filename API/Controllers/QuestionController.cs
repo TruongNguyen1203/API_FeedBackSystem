797bd574151e6a59d35ea7b13d91f7bee715d686
Thuyety <thuyety15@gmail.com>
2021-05-26 17:19:15 +0700

    fix question getall update -v2

--- API/Controllers/QuestionController.cs
+++ API/Controllers/QuestionController.cs
@@ -43,33 +43,48 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public IActionResult GetQuestion(int id)
         {
-            var question =_context.Questions.Find(id);
-            var q= new QuestionDto()
-            {
-                QuestionContent=question.QuestionContent,
-                TopicID=question.TopicID
-            };
-
-            return Ok(q);
+            var question =_context.Questions.Where(x=>x.QuestionID==id)
+                                            .Select(x=> new QuestionDto(){
+                                                QuestionID=x.QuestionID,
+                                                QuestionContent=x.QuestionContent,
+                                                TopicID=x.TopicID
+                                            }).FirstOrDefault();
+            return Ok(question);
         }
         [HttpPut("{update}")]
-        public IActionResult Update([FromBody]Question question)
+        public IActionResult Update([FromBody]QuestionDto questionDto)
         {
+            var question =_context.Questions.Where(x=>x.QuestionID==questionDto.QuestionID)
+                                            .Select(x=> new Question(){
+                                                QuestionID=x.QuestionID,
+                                                Topic=x.Topic,
+                                                IsDeleted=x.IsDeleted,
+                                                Feedback_Questions=x.Feedback_Questions,
+                                                Answers=x.Answers,
+                                                QuestionContent=questionDto.QuestionContent
+                                            }).FirstOrDefault();
             _context.Questions.Update(question);
             _context.SaveChanges();
-            return Ok(new {success=true, message="Add Question success!"}); 
+            return Ok(new {success=true, message="Update Question success!"}); 
         }
         [HttpDelete("delete/{id}")]
         public IActionResult Delete(int id)
         {
             // ooh no
             var question=_context.Questions.Find(id);
-             return Ok(new {success=true, message="Add Question success!",question=question});
+            return Ok(new {success=true, message="Delete Question success!",question=question});
         }
         [HttpGet]
         public IActionResult GetAll()
         {
-            var questions=_context.Questions.Include(x=>x.Topic).ToList();
+            var questions=_context.Questions
+                        .Select(x=>new{
+                            TopicID=x.TopicID,
+                            TopicName=x.Topic.TopicName,
+                            QuestionID=x.QuestionID,
+                            QuestionContent=x.QuestionContent
+                        })
+                        .ToList();
             return Ok(questions);
         }
 
