698afb544fafd6a8fa3698e3bb7deb11a56e7e31
Thuyety <thuyety15@gmail.com>
2021-05-28 12:39:43 +0700

    Execute feedback -v1 without check errors at DB

--- API/Controllers/DashboardController.cs
+++ API/Controllers/DashboardController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using Core.Entities;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -41,7 +43,83 @@ namespace API.Controllers
                                                 EndTime=x.Module.EndTime.ToString("MM/dd/yyyy HH:mm"),
                                                 StatusCode=x.Module.EndTime>DateTime.Now?"Complete":"InComplete"
                                             }).ToListAsync();
-            return Ok(feedbacks);
+             // ====> get feedback which was not completed
+            // => query where Answer has traineeID==.. && moduleID== ... => FEEDBACK COMPLETE
+            var listComplete=feedbacks.Join(_context.Answers,x=>x.ModuleID,y=>y.ModuleID,
+                                        (x,y)=> new{
+                                            ClassID=x.ClassID,
+                                            ModuleID=x.ModuleID,
+                                            FeedbackID=x.FeedbackID,
+                                            TraineeID=y.TraineeID
+                                        })
+                                        .ToList();
+            return Ok(new {listFeedback=feedbacks,listComplete=listComplete});
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetFeedback(int id)
+        {
+            Dictionary<string,List<AnswerDto>> feedback = new Dictionary<string, List<AnswerDto>>();
+            var allTopic= await _context.Topics.Select(x=>x.TopicName).ToListAsync();
+            foreach(var item in allTopic)
+            {
+                feedback.Add(item,await _context.Feedback_Questions.Where(x=>x.FeedbackID==id &&x.Question.Topic.TopicName==item)
+                                                .Select(x=> new AnswerDto(){
+                                                    QuestionID=x.Question.QuestionID,
+                                                    QuestionContent=x.Question.QuestionContent
+                                                }).ToListAsync());
+            }
+            return Ok(feedback.Values);
+        }
+        [HttpPost("do")]
+        public async Task<ActionResult> Execute(int id, string comment,string traineeID, List<AnswerDto> answerDto)
+        {
+            try
+            {
+                 // return Ok(answerDto);
+                var module=await _context.Modules.Where(x=>x.FeedbackID==id).FirstOrDefaultAsync();
+                
+                // get classID to add to Trainee_Comment by join Module and Assignment
+                var classOfTrainee=await _context.Assignments.Where(x=>x.ModuleID==module.ModuleID)
+                                                            .Select(x=>x.Class)
+                                                            .FirstOrDefaultAsync();  
+                // get trainee
+                var trainee=await _context.Trainees.Include(x=>x.AppUser).FirstOrDefaultAsync(x=>x.TraineeID==traineeID);          
+                // add comment
+                if(comment!=null)
+                {
+                    
+                    Trainee_Comment newComment = new Trainee_Comment()
+                    {
+                        ClassID=classOfTrainee.ClassID,
+                        ModuleID=module.ModuleID,
+                        TraineeID=traineeID,
+                        Comment=comment
+                    };
+                    await _context.AddAsync(newComment);
+                    await _context.SaveChangesAsync();
+                }   
+                // add answer
+                List<Answer> newAnswer= new List<Answer>(); 
+                foreach (var item in answerDto)
+                {
+                    newAnswer.Add( new Answer(){
+                        Class=classOfTrainee,
+                        Module=module,
+                        Trainee=trainee,
+                        Question=_context.Questions.Find(item.QuestionID),
+                        Value=item.AnswerValue
+                    });
+                }
+                await _context.AddRangeAsync(newAnswer);
+                await _context.SaveChangesAsync();
+                // return success
+                return Ok(new{success=true, message="Submit Feedback Success"});
+            }
+            catch (Exception e)
+            {
+                return Ok(new {success=false, message=e.ToString()});
+            }
+           
         }
     }
 }
---
+++ API/Dtos/AnswerDto.cs
@@ -0,0 +1,9 @@
+namespace API.Dtos
+{
+    public class AnswerDto
+    {
+        public int QuestionID { get; set; }
+        public string QuestionContent { get; set; }
+        public int AnswerValue { get; set; }
+    }
+}
